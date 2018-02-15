5dab6ba9754336bd6493cfc66e515b7607093211
Sharkbane <xwildshark@hotmail.com>
2018-02-14 20:28:40 -0500

    Set up Dev Keyboard Input
    So I have no idea how to use the inputs in Unity. Sorry about that. I kind of assumed inputs would be set up and I would be converting it to Player movements.

---
+++ Steaming Bricks/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	GameObject player;
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+		player = GameObject.Find ("Player");
+
+		speed = 0.1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MovePlayer ();
+	}
+
+	// uses arrow keys to shift the GameObject identified as "Player" by the speed.
+	void MovePlayer() {
+		Vector3 current = player.transform.position;
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			current += new Vector3 (speed, 0, 0);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			current += new Vector3 (-speed, 0, 0);
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			current += new Vector3 (0, 0, -speed);
+		}
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			current += new Vector3 (0, 0, speed);
+		}
+		player.transform.position = current;
+	}
+
+	/* Theoretically uses an axis to move by an amount relative to the amount of stick motion.
+	void MovePlayerStick() {
+		Vector3 current = player.transform.position;
+		if (Input.GetAxis(?x?)) {
+			current += new Vector3 (speed * Input.GetAxis(x), 0, 0);
+		}
+		if (Input.GetAxis(?y?)) {
+			current += new Vector3 (0, 0, speed * Input.GetAxis(y));
+		}
+		player.transform.position = current;
+	}
+	*/
+}
