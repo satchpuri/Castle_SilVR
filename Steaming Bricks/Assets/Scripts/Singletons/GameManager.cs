cf1296a661ff913d2476008f6c394a171678db4a
Varun Mhatre <varunjmhatre@gmail.com>
2018-02-23 17:21:29 -0500

    Added Game Manager

---
+++ Steaming Bricks/Assets/Scripts/Singletons/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : Singleton<GameManager>
+{
+    //references to movable objects, hands, and player
+
+    public GameObject Player;
+    //Object with tag "Movable"
+    public GameObject[] Movable_Objects;
+    //Object with tag "Sliding"
+    public GameObject[] Sliding_Object;
+
+    // Use this for initialization
+    void Start()
+    {
+        Movable_Objects = GameObject.FindGameObjectsWithTag("Movable");
+        Sliding_Object = GameObject.FindGameObjectsWithTag("Sliding");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
