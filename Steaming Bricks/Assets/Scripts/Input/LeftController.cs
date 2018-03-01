8e1f3339dfa3f81aec61770fab9dee96249a8b88
jayshuart <joelshuart@gmail.com>
2018-02-28 21:07:55 -0500

    basic version of new left controller script

--- Steaming Bricks/Assets/Scripts/Input/LeftController.cs
+++ Steaming Bricks/Assets/Scripts/Input/LeftController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+using UnityEngine.XR;
 
-public class LeftController : MonoBehaviour {
+public class LeftController : BaseController {
 
 	// Use this for initialization
-	void Start () {
+    protected override void Start () {
+        base.Start();
+
+        //set varibles for specific hand from parent
+        handNode = XRNode.LeftHand;
+        triggerAxis = ""; // NOT SET
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+    protected override void Update () {
+        base.Update();
 	}
 }
