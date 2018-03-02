af3446efb335787976f5860828bffcdf46611199
Varun Mhatre <varunjmhatre@gmail.com>
2018-03-01 19:56:05 -0500

    Bug Fix - sliding object clips through wall/obstacles (they'll need to have rigidbody)

--- Steaming Bricks/Assets/Scripts/Interaction Mechanics/SlideObject.cs
+++ Steaming Bricks/Assets/Scripts/Interaction Mechanics/SlideObject.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class SlideObject : MonoBehaviour
 {
-
+    //For object click and slide
     private Vector3 screenPoint;
     private Vector3 offset;
+
+    //To prevent object from moving in the y direction
     private Vector3 initial_pos;
-    private bool droppable = false;
+
+    //NOTE - To prevent object from clipping through obstacles/walls, 
+    //all objects need rigidbody for Physics.Raycast
+
+    //To prevent object from moving through walls
+    private Vector3 prev_pos;
+    private bool forwardBlocked, backwardBlocked, leftBlocked, rightBlocked = false;
+    private float rayDistance;
+    protected RaycastHit hit; //what the raycast hits
 
     //Level walls
     //To be replaced by range of actual floor of level
@@ -27,7 +37,10 @@ public class SlideObject : MonoBehaviour
     {
 		//Set step as distance between 2 nodes in grid manager
 		step = Mathf.RoundToInt(GridManager.Instance.nodeRadius * 2);
-
+        
+        //To check if there is a wall near this object (may need to be edited based on grid and level layout)
+        rayDistance = transform.GetComponent<MeshRenderer>().bounds.extents.z/2 + GridManager.Instance.nodeRadius*3;
+        
         //Set floor (y) to height of the floor plane
         floor = GridManager.Instance.floorPlane.transform.position.y;
     }
@@ -42,13 +55,16 @@ public class SlideObject : MonoBehaviour
     void OnMouseDown()
     {
         GridManager.Instance.curr_object = transform;
-        initial_pos = transform.position;
         screenPoint = Camera.main.WorldToScreenPoint(transform.position);
         offset = transform.position - Camera.main.ScreenToWorldPoint(
             new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
         GetComponent<Collider>().enabled = false;
         GridManager.Instance.UpdateGrid();
         GetComponent<Collider>().enabled = true;
+
+        initial_pos = transform.position;
+        //Set as initial position for initial value
+        prev_pos = transform.position;
     }
 
     //For object move
@@ -61,32 +77,115 @@ public class SlideObject : MonoBehaviour
 
         //To ensure object doesnt phase through floor 
         //Adding physics makes this redundant
-		if (curPosition.y < floor)
+        if (curPosition.y < floor)
         {
             curPosition.y = floor;
         }
 
         //Store y value so that object can still move in that direction when it hits castle wall
-        float y = curPosition.y;
+        //float y = curPosition.y;
 
-        //To snap object into position where it can be dropped
-        
+        //To slide objects in steps in the grid
+
         curPosition.x = ((int)Mathf.Round(curPosition.x) / step) * step;
         curPosition.z = ((int)Mathf.Round(curPosition.z) / step) * step;
-        droppable = true;
-       
+
 
         //So that it doesn't lift off ground
         curPosition.y = initial_pos.y;
+        
+        //Prevent object clipping begin--------------
+        //To check is there an obsatcle to the right of the object 
+        if (Physics.Raycast(transform.position, -transform.right, out hit, rayDistance))
+        {
+            if (hit.rigidbody.gameObject.layer == 8 && (curPosition.x - transform.position.x < 0))
+            {
+                leftBlocked = true;
+            }
+            else
+            {
+                leftBlocked = false;
+            }
+        }
+        else
+        {
+            leftBlocked = false;
+        }
 
-        //Condition for when you hit castle wall. Redundant with physics?
+        //To the left
+        if (Physics.Raycast(transform.position, transform.right, out hit, rayDistance))
+        {
+            //If hit obsatcle is a part of the filled layer (may not be required)
+            //Also if new slidong object position is going toward or away from the hit obsatcle
+            if (hit.rigidbody.gameObject.layer == 8 && (curPosition.x - transform.position.x > 0))
+            {
+                rightBlocked = true;
+            }
+            else
+            {
+                rightBlocked = false;
+            }
+        }
+        else
+        {
+            rightBlocked = false;
+        }
+
+        //In front of
+        if (Physics.Raycast(transform.position, transform.forward, out hit, rayDistance))
+        {
+            if (hit.rigidbody.gameObject.layer == 8 && (curPosition.z - transform.position.z > 0))
+            {
+                forwardBlocked = true;
+            }
+            else
+            {
+                forwardBlocked = false;
+            }
+        }
+        else
+        {
+            forwardBlocked = false;
+        }
+
+        //Behind
+        if (Physics.Raycast(transform.position, -transform.forward, out hit, rayDistance))
+        {
+            if (hit.rigidbody.gameObject.layer == 8 && (curPosition.z - transform.position.z < 0))
+            {
+                backwardBlocked = true;
+            }
+            else
+            {
+                backwardBlocked = false;
+            }
+        }
+        else
+        {
+            backwardBlocked = false;
+        }
+
+        //Lock object in x direction, prevent it from moving through the wall
+        if (leftBlocked || rightBlocked)
+        {
+            curPosition.x = prev_pos.x;
+        }
+
+        //Lock object in z direction
+        if (forwardBlocked || backwardBlocked)
+        {
+            curPosition.z = prev_pos.z;
+        }
+        //Prevent object clipping end--------------
+
+        //Condition for when you hit castle wall
         if (GridManager.Instance.NodePoint(curPosition).free && curPosition.x <= rangeA.x && curPosition.x >= rangeB.x && curPosition.z <= rangeA.z && curPosition.z >= rangeB.z)
         {
-            //To prevent clipping. 
-            //Draw line between object and mouse position.y. 
-            //If there are !free objects there, don't move at all.
+            //update position
             transform.position = curPosition;
         }
+        //Store previos position for next cycle
+        prev_pos = curPosition;
 
     }
 
