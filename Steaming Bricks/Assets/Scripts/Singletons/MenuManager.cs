4eeac4aa6eace79b5e0c57b0cc8ac59f41def902
Satch Puri <sp1796@g.rit.edu>
2018-03-08 17:33:01 -0500

    Start button working. Base menuManager

---
+++ Steaming Bricks/Assets/Scripts/Singletons/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : Singleton<MenuManager> {
+
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Working");
+
+        if (Input.GetKeyDown("space"))
+            print("space key was pressed");
+
+    }
+}
