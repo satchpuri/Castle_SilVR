60092810e10b542338812c51659a05c8fd01a76b
Erin McAnany <elm6208@rit.edu>
2018-03-23 04:12:33 -0400

    Guards completed
    Added GuardMovement and GuardVision scripts, added Erin scene with a test guard.

---
+++ Steaming Bricks/Assets/Scripts/GuardMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GuardMovement : MonoBehaviour {
+
+    //waypoints for guard to follow
+    public Transform[] waypoints;
+    //current waypoint they are seeking
+    private int currWaypointNum;
+    //movement speed
+    public float speed;
+    //vision cone
+    private GameObject vision;
+    //player
+    private GameObject player;
+
+    // Use this for initialization
+    void Start () {
+        //start on waypoint 0
+        currWaypointNum = 0;
+
+        //get child vision object
+        vision = this.gameObject.transform.GetChild(0).gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        SeekPoint(waypoints[currWaypointNum]);
+	}
+
+    //moves guard towards current waypoint, updates current waypoint
+   private void SeekPoint(Transform waypointLoc)
+    {
+        //move towards point
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, waypointLoc.position, step);
+
+        //turn to face point, movement is somewhat unnatural, may need to be edited later
+        var targetPoint = waypointLoc.transform.position;
+        var targetRotation = Quaternion.LookRotation(targetPoint - transform.position, Vector3.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2.0f);
+
+        //calculate distance to waypoint
+        float dist = Vector3.Distance(this.transform.position, waypointLoc.transform.position);
+
+        //if it reaches the location, cycle to the next waypoint
+        if (dist < 1)
+        {
+            currWaypointNum += 1;
+            if(currWaypointNum >= waypoints.Length)
+            {
+                currWaypointNum = 0;
+            }
+        }
+    }
+
+    
+
+}
---
+++ Steaming Bricks/Assets/Scripts/GuardVision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//this script should go on the vision cone itself
+
+public class GuardVision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //for colliding with player
+    void OnTriggerEnter(Collider other)
+    {
+        //on collision with player, reload scene
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
