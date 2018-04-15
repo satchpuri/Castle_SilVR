66f2c105218a9884c1820d5a776006699ab2e4e4
jayshuart <joelshuart@gmail.com>
2018-04-15 12:58:11 -0400

    cleared some old scenes, added proper paths, updated how paths are made

--- Steaming Bricks/Assets/Scripts/GuardMovement.cs
+++ Steaming Bricks/Assets/Scripts/GuardMovement.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class GuardMovement : MonoBehaviour {
 
     //waypoints for guard to follow
-    public Transform[] waypoints;
+    [SerializeField] private GameObject pathParent;
+    public List<Transform> waypoints;
     //current waypoint they are seeking
     private int currWaypointNum;
     //movement speed
@@ -17,27 +18,34 @@ public class GuardMovement : MonoBehaviour {
     private GameObject player;
     //check if guard is distracted
     public bool distracted = false;
+
+
     // Use this for initialization
     void Start () {
+        //build path from GO that hold spath nodes
+        waypoints.AddRange(pathParent.GetComponentsInChildren<Transform>());
+        waypoints.RemoveAt(0); //remove 0 index because GetComponentsInCHildren is inclusive of parent
+
         //start on waypoint 0
         currWaypointNum = 0;
 
-        if(waypoints.Length > 0)
-        {
-            //get child vision object
-            vision = this.gameObject.transform.GetChild(0).gameObject;
-        }
-        
+        //get child vision object
+        vision = this.gameObject.transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!distracted && waypoints.Length > 0)
+        if (!distracted && waypoints.Count > 0)
         {
             SeekPoint(waypoints[currWaypointNum]);
         }
 	}
+
+    private void BuildPath()
+    {
+        
+    }
 
     //moves guard towards current waypoint, updates current waypoint
    private void SeekPoint(Transform waypointLoc)
@@ -58,7 +66,7 @@ public class GuardMovement : MonoBehaviour {
         if (dist < .01f)
         {
             currWaypointNum += 1;
-            if(currWaypointNum >= waypoints.Length)
+            if(currWaypointNum >= waypoints.Count)
             {
                 currWaypointNum = 0;
             }
