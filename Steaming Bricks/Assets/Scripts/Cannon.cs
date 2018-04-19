39d2792ee8248e399101659993daafda3d61bb4a
Satch Puri <sp1796@g.rit.edu>
2018-04-19 02:03:28 -0400

    Magic Hands

---
+++ Steaming Bricks/Assets/Scripts/Cannon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour {
+
+    public GameObject projectile;
+    public float shootForce = 500.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject cannonball = (GameObject)Instantiate(
+                projectile, transform.position, transform.rotation);
+            projectile.GetComponent<Rigidbody>().AddForce(cannonball.transform.forward * shootForce);
+        }
+		
+	}
+}
