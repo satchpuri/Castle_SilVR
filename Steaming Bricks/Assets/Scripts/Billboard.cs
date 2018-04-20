729aa2b96553b69954590da97f80241581dfb2db
jayshuart <joelshuart@gmail.com>
2018-04-19 20:09:30 -0400

    UI inidcators part 1

---
+++ Steaming Bricks/Assets/Scripts/Billboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //hide initally
+        this.gameObject.SetActive(false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //turn to face the player
+        this.gameObject.transform.LookAt(Camera.main.transform);
+		
+	}
+}
