5bad12822251e8dc55f135b1747f92f34fdad1a0
Erin McAnany <elm6208@rit.edu>
2018-04-24 21:49:43 -0400

    Created End Door prefab
    Created End Door prefab. It is supposed to switch levels when the Player collides with it, however the scenes may not currently be added to the build manager properly.

---
+++ Steaming Bricks/Assets/Scripts/Interaction Mechanics/EndDoor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndDoor : MonoBehaviour {
+
+    //temp Scene changing until we figure SceneManager issue out
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //trigger next scene when player enters door
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            //get current scene index and up by 1 - using mod t wrap back to 0 at end of scene list
+            int nextIndex = SceneManager.GetActiveScene().buildIndex + 1 % SceneManager.sceneCountInBuildSettings;
+
+            if(nextIndex == 3)
+            {
+                SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(0));
+            }
+            else
+            {
+                //goto that scene
+                //Scene scene = SceneManager.LoadSceneAsync(nextIndex);
+                SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(nextIndex));
+            }
+            
+            
+        }
+    }
+}
