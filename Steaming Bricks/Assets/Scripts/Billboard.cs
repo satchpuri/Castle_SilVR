a551f5f60234c5fd05cbeffadaade8f21c8dc4a3
Sharkbane <xwildshark@hotmail.com>
2018-04-24 23:07:47 -0400

    Billboard Changes and player indicator

--- Steaming Bricks/Assets/Scripts/Billboard.cs
+++ Steaming Bricks/Assets/Scripts/Billboard.cs
@@ -7,15 +7,15 @@ public class Billboard : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         //hide initally
-        this.gameObject.SetActive(false);
-		
+        this.gameObject.SetActive(false);		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         //turn to face the player
-        this.gameObject.transform.LookAt(Camera.main.transform);
-		
+		//this.gameObject.transform.LookAt(Camera.main.transform,Vector3.up);
+		transform.rotation = Quaternion.LookRotation (Camera.main.transform.position, Vector3.up);
+
 	}
 }
---
+++ Steaming Bricks/Assets/Scripts/PlayerIndicator.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIndicator : MonoBehaviour {
+
+	bool indicatorOn = false;
+
+	Vector3 thisFramePos;
+	Vector3 lastFramePos;
+
+	float timeToIndicate = 2;
+
+	float time;
+	float moveTime = 0;
+
+	GameObject indicator;
+
+	// Use this for initialization
+	void Start () {
+		time = 0;
+		indicator = transform.GetChild (0).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		lastFramePos = thisFramePos;
+		thisFramePos = transform.position;
+
+		if (!indicatorOn && lastFramePos == thisFramePos) {
+
+			time += Time.deltaTime;
+
+			if (time >= timeToIndicate) {
+				
+				ShowIndicator ();
+			}
+		} else if (indicatorOn && lastFramePos != thisFramePos) {
+
+			time = 0;
+			HideIndicator ();
+		}
+
+		if (indicatorOn && moveTime <= .1f) {
+
+			moveTime += Time.deltaTime;
+
+			indicator.transform.position = new Vector3 (transform.position.x, transform.position.y + (moveTime / 2), transform.position.z);
+
+			Debug.Log (moveTime);
+		}
+	}
+
+	private void ShowIndicator() {
+
+		indicator.SetActive (true);
+		indicatorOn = true;
+	}
+
+	private void HideIndicator() {
+
+		indicator.SetActive (false);
+		indicatorOn = false;
+		moveTime = 0;
+	}
+}
