072d95c67ebd0edf06e88bba4d9b40b3d6e336e2
Sharkbane <xwildshark@hotmail.com>
2018-04-26 16:41:19 -0400

    update billboard & clean my old scenes

--- Steaming Bricks/Assets/Scripts/Billboard.cs
+++ Steaming Bricks/Assets/Scripts/Billboard.cs
@@ -13,9 +13,24 @@ public class Billboard : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		//check if tiny terry is close
+
+
         //turn to face the player
 		//this.gameObject.transform.LookAt(Camera.main.transform,Vector3.up);
-		transform.rotation = Quaternion.LookRotation (Camera.main.transform.position, Vector3.up);
+		transform.rotation = Quaternion.LookRotation (Camera.main.transform.position, transform.parent.transform.up);
+
+	}
+
+	private void CheckActivate() {
+
+		if (!gameObject.activeSelf && ((GameObject.Find ("Player").transform.position - transform.position).magnitude < .01f)) {
+
+			gameObject.SetActive (true);
+		} else if (gameObject.activeSelf && ((GameObject.Find ("Player").transform.position - transform.position).magnitude >= .01f)) {
+
+			gameObject.SetActive (false);
+		}
 
 	}
 }
