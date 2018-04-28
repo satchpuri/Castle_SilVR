604531a04913d6308890ca090e97afce5f016865
Sharkbane <xwildshark@hotmail.com>
2018-04-27 22:41:36 -0400

    arrows prefabs and script

---
+++ Steaming Bricks/Assets/Scripts/SlideArrowController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideArrowController : MonoBehaviour {
+
+	private SpriteRenderer arrows;
+
+	// Use this for initialization
+	void Start () {
+
+		arrows = transform.GetChild (0).GetComponent<SpriteRenderer> ();
+		arrows.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.GetComponentInParent<MoveObject> ().activeSliding) {
+
+			arrows.enabled = true;
+		} else {
+
+			arrows.enabled = false;
+		}
+	}
+}
